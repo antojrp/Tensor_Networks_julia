454fa8299b8aba3ebb9a230ee5e0613cc22f1b1d
antojrp <ajesusrivera@gmail.com>
2025-03-17 11:15:18 +0100

    a

--- Programas/GPU-CPU.jl
+++ Programas/GPU-CPU.jl
@@ -4,12 +4,12 @@ using ITensorMPS
 using Random
 using .Threads
 
-function imprimir(time,io1)
+function imprimir(time,io1,nsim,ndims,D_i,paso)
     tiempos=zeros(ndims)
     total=zeros(nsim)
     media=0
     for i in 1:ndims
-      write(io1,"$(dim_i+(i-1)*pasod))")
+      write(io1,"$(D_i+(i-1)*paso)")
       for k in 1:nsim
         tiempos[i]=tiempos[i]+time[k,i]
         total[k]=total[k]+time[k,i]
@@ -39,38 +39,41 @@ end
 let
   D_i=100
   paso=100
-  ndim=10
+  ndims=10
   nsim=10
-  io1 = open("resultados/DMRG_energia_D$(D_i)_t$(BLAS.get_num_threads()).txt","w")
+  io1 = open("resultados/GPU-CPU_energia_t$(BLAS.get_num_threads()).txt","w")
   close(io1)
 
-
+  time=zeros(nsim,ndims)
   for i in 1:ndims
-    dim=dim_i+(i-1)*pasod
-    io1 = open("resultados/DMRG_energia_D$(D_i)_t$(BLAS.get_num_threads()).txt","a")
+    dim=D_i+(i-1)*paso
+    io1 = open("resultados/GPU-CPU_energia_t$(BLAS.get_num_threads()).txt","a")
     write(io1,"D   Time  var(Time)  \n")
-    time=zeros(nsim,ndims)
-    i, j, k = Index.((dim, dim, dim))
+    l = Index(dim)
+    m = Index(dim)
+    n = Index(dim)
     for k in 1:nsim
-      A = random_itensor(i, j)
-      B = random_itensor(j, k)
+      A = random_itensor(l, m)
+      B = random_itensor(m, n)
       t= @elapsed A * B
       time[k,i]=t
     end
   end
-  imprimir(time,io1)
+  imprimir(time,io1,nsim,ndims,D_i,paso)
   close(io1)
   
   using CUDA # This will trigger the loading of `NDTensorsCUDAExt` in the 
+  time=zeros(nsim,ndims)
   for i in 1:ndims
-    dim=dim_i+(i-1)*pasod
-    io1 = open("resultados/DMRG_energia_D$(D_i)_t$(BLAS.get_num_threads()).txt","a")
+    dim=D_i+(i-1)*paso
+    io1 = open("resultados/GPU-CPU_energia_t$(BLAS.get_num_threads()).txt","a")
     write(io1,"D   Time  var(Time)  \n")
-    time=zeros(nsim,ndims)
-    i, j, k = Index.((dim, dim, dim))
+    l = Index(dim)
+    m = Index(dim)
+    n = Index(dim)
     for k in 1:nsim
-      A = random_itensor(i, j)
-      B = random_itensor(j, k)
+      A = random_itensor(l, m)
+      B = random_itensor(m, n)
       Acu = cu(A)
       Bcu = cu(B)
       t=@elapsed Acu * Bcu
