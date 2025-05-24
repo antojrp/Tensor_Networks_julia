0a59e2087d50be7a7a16d5ce8790f73b933458c1
antojrp <ajesusrivera@gmail.com>
2025-05-24 20:29:39 +0200

    Resultados DMRG

--- Programas/DMRG.jl
+++ Programas/DMRG.jl
@@ -37,26 +37,25 @@ let
 
   ITensors.disable_warn_order() 
   N=50
-  nsim=10
+  nsim=20
   nsweeps=10
   nm=21
   J=1
-  p=5
+  p=8
   maxdim=2^p
 
   cutoff = [1E-15]
 
-  io1 = open("resultados/DMRG_L$(N)_$(p).txt","w")
+  io1 = open("resultados/DMRG_L$(N)_8.txt","w")
   write(io1,"Gamma, E, varE, T, varT, dimension, vardimension \n")
   close(io1)
 
   for gamma in range(0, stop=2, length=nm)
 
-    io1 = open("resultados/DMRG_L$(N)_$(p).txt","a")
+    io1 = open("resultados/DMRG_L$(N)_8.txt","a")
     print("Numero de qubits: $N \n")
-    print("Dimension max: $dim \n")
-    print("Threads (LinearAlgebra) :$(BLAS.get_num_threads())\n")
-    print("Threads (Julia) :$(Threads.nthreads())\n\n\n")
+    print("Dimension max: $maxdim \n")
+    print("p= $p \n")
 
     sites = siteinds("Qubit",N)
     os1 = OpSum()
