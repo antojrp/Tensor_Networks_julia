07e96a0a0bb97018e5550dbaaa564a8ea5d9ada9
antojrp <ajesusrivera@gmail.com>
2025-10-30 10:45:37 +0100

    nuevos datos

--- pTEBD/random_circuit_sim.sh
+++ pTEBD/random_circuit_sim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
  
-#SBATCH -c 30
+#SBATCH -c 10
 #SBATCH -p GPGPU
 #SBATCH --mem=100GB
 #SBATCH -t 72:00:00 
 
-OMP_NUM_THREADS=5 /home/ajrp/julia-1.11.1/bin/julia -t 5 --heap-size-hint=45G random_circuit_sim.jl
+OMP_NUM_THREADS=4 /home/ajrp/julia-1.11.1/bin/julia -t 1 --heap-size-hint=45G random_circuit_sim.jl
