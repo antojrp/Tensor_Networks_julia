77628b1f2a38f908676664a1c8a66b40c438eac9
antojrp <ajesusrivera@gmail.com>
2025-11-03 12:56:43 +0100

    cambio

--- pTEBD/random_circuit_sim.sh
+++ pTEBD/random_circuit_sim.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=100GB
 #SBATCH -t 72:00:00 
 
-OMP_NUM_THREADS=4 /home/ajrp/julia-1.11.1/bin/julia -t 3 random_circuit_sim.jl
+OMP_NUM_THREADS=8 /home/ajrp/julia-1.11.1/bin/julia -t 2 random_circuit_sim.jl
